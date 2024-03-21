535a59c7c8326f440856121cf3f3d5ba224c0794
Xiaotong Xu <102460988+ntxxt@users.noreply.github.com>
2024-03-21 12:19:53 +0100

    enable custom input chainIDs

--- scripts/get_fasta.py
+++ scripts/get_fasta.py
@@ -1,51 +1,54 @@
+import glob
 import argparse
-import os
-from pdb2sql import pdb2sql
+from Bio.PDB import PDBParser
+from Bio.PDB.Polypeptide import is_aa
+import warnings
+from Bio import BiopythonWarning
 
-One2ThreeDict = {'A': 'ALA', 'R': 'ARG', 'N': 'ASN', 'D': 'ASP', 'C': 'CYS', 'E': 'GLU', 'Q': 'GLN',
-                 'G': 'GLY', 'H': 'HIS', 'I': 'ILE', 'L': 'LEU', 'K': 'LYS', 'M': 'MET', 'F': 'PHE',
-                 'P': 'PRO', 'S': 'SER', 'T': 'THR', 'W': 'TRP', 'Y': 'TYR', 'V': 'VAL',
-                 'B': 'ASX', 'U': 'SEC', 'Z': 'GLX'}
+def three_to_one(code):
+    """Convert three-letter amino acid code to one-letter code."""
+    aa_map = {
+        'ALA': 'A', 'ARG': 'R', 'ASN': 'N', 'ASP': 'D', 'CYS': 'C',
+        'GLN': 'Q', 'GLU': 'E', 'GLY': 'G', 'HIS': 'H', 'ILE': 'I',
+        'LEU': 'L', 'LYS': 'K', 'MET': 'M', 'PHE': 'F', 'PRO': 'P',
+        'SER': 'S', 'THR': 'T', 'TRP': 'W', 'TYR': 'Y', 'VAL': 'V'
+    }
+    return aa_map.get(code, 'X')  # Return 'X' if the code is not found
 
-Three2OneDict = {v: k for k, v in One2ThreeDict.items()}
+def pdb_to_fasta(pdb_file_dir, chain_id1, chain_id2):
+    """Convert a PDB file to a FASTA file."""
 
+    sample_pdb = glob.glob(f'{pdb_file_dir}/*.pdb')[0]
 
-def get_fasta(pdb_path, output_fasta_dir):
-    sqldb = pdb2sql(pdb_path)
-    
-    for chain_id in ('A', 'B'):
-        residues = sqldb.get_residues(chainID=chain_id)
-        seq = ''.join(Three2OneDict[residue[1]] for residue in residues)
-        
-        case_id = os.path.basename(pdb_path).split('.')[0]
-        output_file = os.path.join(output_fasta_dir, f'{case_id}.{chain_id}.fasta')
+    pdb_id = sample_pdb.split("/")[-1].split(".")[0]
 
-        with open(output_file, 'w') as f:
-            f.write(f'>{case_id}.{chain_id}\n')
-            f.write('\n'.join([seq[i:i+79] for i in range(0, len(seq), 79)]))
+    parser = PDBParser()
+    structure = parser.get_structure("structure", sample_pdb)
 
+    with open(f"{pdb_id}_merged.fasta", "w") as file:
+        for chain_id in [chain_id1, chain_id2]:
+            chain = structure[0][chain_id]
+            sequence = ""
 
-def combine_fasta_files(fasta_dir, output_fasta_name):
-    os.system(f"sed -n 'p' {fasta_dir}/*.fasta > {output_fasta_name}")
+            # Get the sequence of the chain
+            for residue in chain:
+                if not is_aa(residue.get_resname()):
+                    continue
+                sequence += three_to_one(residue.get_resname())
 
+            # Write the sequence to the file with chain ID
+            file.write(f">{chain_id}\n")
+            file.write(sequence + "\n")
 
-def main(pdb_dir, output_fasta_name):
-    fasta_dir = os.path.join(pdb_dir, 'fasta_files')
-    os.makedirs(fasta_dir, exist_ok=True)
-
-    for pdb_file in os.listdir(pdb_dir):
-        if pdb_file.endswith('.pdb'):
-            pdb_path = os.path.join(pdb_dir, pdb_file)
-            get_fasta(pdb_path, output_fasta_dir=fasta_dir)
-
-    combine_fasta_files(fasta_dir, output_fasta_name)
-    print('Fasta files generated for all PDB files')
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('pdb_dir', help='Path to the directory containing PDB files')
-    parser.add_argument('output_fasta_name', help='Name of the combined output FASTA file')
+def main():
+    parser = argparse.ArgumentParser(description='Convert a PDB file to a FASTA file for specified chains.')
+    parser.add_argument('pdb_file_path', type=str, help='Path to the PDB file directory')
+    parser.add_argument('chain_id1', type=str, help='Chain ID for the first sequence')
+    parser.add_argument('chain_id2', type=str, help='Chain ID for the second sequence')
     args = parser.parse_args()
 
-    main(args.pdb_dir, args.output_fasta_name)
+    warnings.simplefilter('ignore', BiopythonWarning)
+    pdb_to_fasta(args.pdb_file_path, args.chain_id1, args.chain_id2)
+
+if __name__ == "__main__":
+    main()
